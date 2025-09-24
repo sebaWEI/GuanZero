2d34b017faf4c1d0a41c0afa3bfa613de386e3d0
sebaWEI <sebastianwei2023@163.com>
2025-09-24 20:12:39 +0800

    Update evaluate.py

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -1,8 +1,8 @@
 from .simulation import evaluate
 
 if __name__ == '__main__':
-	# Windows 多进程必须放在 main 保护下
-	# 可自行修改为你需要的权重/数据/进程数
+	# Windows multiprocessing must be under main protection
+	# You can modify the weights/data/process count as needed
 	model_path = 'guanzero_checkpoints/second_train/guandan_weights_649600.ckpt'
 	eval_data = 'evaluation/eval.pkl'
 	player_config = {
